f7bde922446f847d25ce0405d6fee0e32511d727
Soumya Parida <soumyaparida00@gmail.com>
2024-07-03 23:41:48 +0530

    Final Commit2

--- variables.tf
+++ variables.tf
@@ -1,10 +1,14 @@
-variable "region" {    
-    default = "ap-south-1"
+# AWS Region
+variable "region" {
+  default = "ap-south-1"
 }
+
+# VPC Configuration
 variable "vpc_cidr" {
   description = "The CIDR block for the VPC"
   default     = "10.6.0.0/16"
 }
+
 variable "private_subnet_cidrs" {
   description = "A list of CIDR blocks for private subnets"
   default     = ["10.6.1.0/24", "10.6.2.0/24", "10.6.3.0/24"]
@@ -12,69 +16,84 @@ variable "private_subnet_cidrs" {
 
 variable "public_subnet_cidrs" {
   description = "A list of CIDR blocks for public subnets"
-  default     = ["10.6.101.0/24", "10.6.102.0/24", "10.6.102.0/24"]
+  default     = ["10.6.101.0/24", "10.6.102.0/24", "10.6.103.0/24"]
 }
 
 variable "availability_zones" {
   description = "A list of availability zones for the subnets"
   default     = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
 }
+
+# EC2 Key Pair
 variable "key_name" {
   description = "The name of the EC2 key pair"
-  default     = "id_rsa" # Change to your EC2 key pair name
+  default     = "id_rsa"
 }
+
+# IAM Role for EKS Node Group
 variable "node_role_arn" {
   description = "The IAM role ARN for the EKS node group"
-  default = "arn:aws:iam::851725524405:role/AmazonEKSNodeRole"
+  default     = "arn:aws:iam::851725524405:role/AmazonEKSNodeRole"
 }
+
+# S3 Bucket for Application
 variable "bucket_name" {
   description = "The name of the S3 bucket"
   default     = "react-app-bucket"
 }
+
+# CloudFront Distribution Aliases
 variable "aliases" {
   description = "The CloudFront distribution aliases"
   default     = ["test.virtualtechbox.com"]
 }
+
+# Backend S3 Bucket for Terraform State
 variable "backend_bucket" {
   description = "The S3 bucket for storing Terraform state"
   default     = "terraform-vtb"
 }
+
+# Backend Key for Terraform State
 variable "backend_key" {
   description = "The key for storing the Terraform state in S3"
   default     = "terraform/terraform.tfstate"
 }
+
+# DynamoDB Table for State Locking
 variable "dynamodb_table_name" {
   description = "The name of the DynamoDB table for state locking"
   default     = "terraform-state-lock"
 }
+
+# Environment Tag
 variable "environment" {
   description = "The environment for which the resources are created"
   default     = "dev"
 }
+
+# IAM Role for EKS Cluster Permissions
 variable "cluster_role_arn" {
   description = "The ARN of the IAM role that provides permissions for the EKS cluster."
   type        = string
-  default = "arn:aws:iam::851725524405:role/eksClusterRole"
+  default     = "arn:aws:iam::851725524405:role/eksClusterRole"
 }
+
+# Subnet IDs for EKS Cluster and Node Groups
 variable "subnet_ids" {
   description = "A list of subnet IDs where the EKS cluster and node groups will be deployed."
   type        = list(string)
   default     = []
 }
 
-locals {
-  private_subnet_ids = aws_subnet.private[*].id
-  public_subnet_ids  = aws_subnet.public[*].id
-
-  all_subnets = concat(local.private_subnet_ids, local.public_subnet_ids)
-}
-
+# EC2 Instance Types for EKS Node Group
 variable "instance_types" {
   description = "List of EC2 instance types for the EKS node group."
   type        = list(string)
   default     = ["t3.medium"]
 }
 
+# Autoscaling Settings for EKS Node Group
 variable "node_desired_size" {
   description = "Desired number of nodes in the EKS node group."
   type        = number
@@ -93,12 +112,14 @@ variable "node_min_size" {
   default     = 1
 }
 
+# Tags for Resources
 variable "tags" {
   description = "A map of tags to add to all resources."
   type        = map(string)
   default     = {}
 }
 
+# IAM Policy JSON for AWS Load Balancer Controller
 variable "load_balancer_controller_policy_json" {
   description = "The JSON policy document for the AWS Load Balancer Controller IAM policy."
   type        = string
