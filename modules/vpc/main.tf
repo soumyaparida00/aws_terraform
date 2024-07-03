fcee6e8d5f708fc3da010ee0432ca69d4a8c2fce
Soumya Parida <soumyaparida00@gmail.com>
2024-07-04 01:16:12 +0530

    Final Commit5

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,30 +1,31 @@
-resource "aws_vpc" "main" {
+resource "aws_vpc" "this" {
   cidr_block = var.vpc_cidr
 
   tags = {
-    Name = "main-vpc"
-  }
-}
-
-resource "aws_subnet" "private" {
-  count             = length(var.private_subnet_cidrs)
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = element(var.private_subnet_cidrs, count.index)
-  availability_zone = element(var.availability_zones, count.index)
-
-  tags = {
-    Name = "private-subnet-${count.index + 1}"
+    Name = "vpc"
   }
 }
 
 resource "aws_subnet" "public" {
-  count             = length(var.public_subnet_cidrs)
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = element(var.public_subnet_cidrs, count.index)
-  availability_zone = element(var.availability_zones, count.index)
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = var.availability_zones[count.index]
+  map_public_ip_on_launch = true
 
   tags = {
-    Name = "public-subnet-${count.index + 1}"
+    Name = "public-subnet-${count.index}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count                   = length(var.private_subnet_cidrs)
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.private_subnet_cidrs[count.index]
+  availability_zone       = var.availability_zones[count.index]
+
+  tags = {
+    Name = "private-subnet-${count.index}"
   }
 }
 
--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,9 +1,9 @@
 output "public_subnet_ids" {
-  value = aws_subnet.public.*.id
+  value = aws_subnet.public[*].id
 }
 
 output "private_subnet_ids" {
-  value = aws_subnet.private.*.id
+  value = aws_subnet.private[*].id
 }
 
 output "vpc_id_output" {
