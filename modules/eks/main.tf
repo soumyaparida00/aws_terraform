14736f57da92c015d841832f3515a590cef481c6
Soumya Parida <soumyaparida00@gmail.com>
2024-07-09 00:05:14 +0530

    eks add ons1

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -24,7 +24,7 @@ resource "aws_eks_cluster" "this" {
 resource "aws_eks_addon" "vpc_cni" {
   cluster_name   = aws_eks_cluster.this.name
   addon_name     = "vpc-cni"
-  addon_version  = "v1.12.1-eksbuild.1"
+  addon_version  = "v1.18.1-eksbuild.3"
   resolve_conflicts = "OVERWRITE"
 
   depends_on = [aws_eks_cluster.this]
@@ -33,7 +33,7 @@ resource "aws_eks_addon" "vpc_cni" {
 resource "aws_eks_addon" "kube_proxy" {
   cluster_name   = aws_eks_cluster.this.name
   addon_name     = "kube-proxy"
-  addon_version  = "v1.24.1-eksbuild.1"
+  addon_version  = "v1.30.0-eksbuild.3"
   resolve_conflicts = "OVERWRITE"
 
   depends_on = [aws_eks_cluster.this]
@@ -42,7 +42,7 @@ resource "aws_eks_addon" "kube_proxy" {
 resource "aws_eks_addon" "coredns" {
   cluster_name   = aws_eks_cluster.this.name
   addon_name     = "coredns"
-  addon_version  = "v1.8.7-eksbuild.1"
+  addon_version  = "v1.11.1-eksbuild.8"
   resolve_conflicts = "OVERWRITE"
 
   depends_on = [aws_eks_cluster.this]
